9064bc7626116bff45c355f7dd8d952862621a8d
NoorTabanjeh <112004945+NoorTabanjeh@users.noreply.github.com>
2022-08-24 12:30:12 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/****************************
+* Hello.java
+*****************************
+* Noor Tabanjeh
+* 08/24/22
+* CMSC-255-004
+*****************************/
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("<Noor Tabanjeh>");
+		System.out.println("<Hello.java>");
+		System.out.println("<This program will run my first lab>");
+		System.out.println("<08/24/2022>");
+		System.out.println("<CMSC-255-004>");
+		System.out.println();
+		
+	}
+}
